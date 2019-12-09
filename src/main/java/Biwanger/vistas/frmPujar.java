902d8c76b2f377032a6ad0d88c569e643475bf78
unknown <sara.olaizola@opendeusto.es>
2019-12-09 08:53:05 +0100

    volver en frmVentaJugador y frmPujar

--- src/main/java/Biwanger/vistas/frmPujar.java
+++ src/main/java/Biwanger/vistas/frmPujar.java
@@ -5,7 +5,9 @@ import Biwanger.objetosDominio.clsJugador;
 import Biwanger.objetosDominio.clsPuja;
 import Biwanger.objetosDominio.clsUsuario;
 
+import javax.swing.border.Border;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class frmPujar extends JFrame
@@ -13,38 +15,44 @@ public class frmPujar extends JFrame
     private clsJugador jugadorSeleccionado;
 
 
-    private int altura = 550;
-    private int anchura = 950;
+    private int altura = 800;
+    private int anchura = 1066;
     private int x = 100;
     private int y = 100;
 
     private JList ListJugadores;
     private JScrollPane scrollListJugadores;
     private modelJugadores modelJugadores;
+    private JPanel pPrincipal, pBotonera;
 
     private JLabel lblSuma;
     private JTextField txtSuma;
 
-    private JButton btnPujar;
+    private JButton btnPujar, btnVolver;
+    private JFrame panelUsuario;
 
-    public frmPujar(clsController controller, clsUsuario usuario)
+    public frmPujar(JFrame frame,clsController controller, clsUsuario usuario)
     {
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        panelUsuario = frame;
 
-        //Definimos el tamano y la localizacion central en la pantalla
+        setUndecorated(true);
+        //setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setSize(anchura, altura);
-        this.setLocation(x, y);
+        //this.setLocation(x, y);
         setResizable(false);
         setTitle("Biwanger Fantasy - Pujar");
-        getContentPane().setLayout(null);
+        getContentPane().setLayout(new BorderLayout(0, 0));
 
         modelJugadores = new modelJugadores(controller.MostrarMercado());
         ListJugadores = new JList(modelJugadores);
 
+        pPrincipal = new JPanel();
+        pPrincipal.setLayout(null);
+        getContentPane().add(pPrincipal,BorderLayout.CENTER);
+
         scrollListJugadores = new JScrollPane();
         scrollListJugadores.setBounds(0, 0, 300, 510);
-        getContentPane().add(scrollListJugadores);
-
+        pPrincipal.add(scrollListJugadores);
         scrollListJugadores.add(ListJugadores);
 
         lblSuma = new JLabel("Introduce la suma de la puja: ");
@@ -53,13 +61,20 @@ public class frmPujar extends JFrame
         txtSuma = new JTextField();
         txtSuma.setBounds(550, 110, 225, 30);
 
-        getContentPane().add(lblSuma);
-        getContentPane().add(txtSuma);
+        pPrincipal.add(lblSuma);
+        pPrincipal.add(txtSuma);
 
         btnPujar = new JButton("Pujar");
         btnPujar.setBounds(605, 280, 115, 30);
         btnPujar.setEnabled(false);
-        getContentPane().add(btnPujar);
+        pPrincipal.add(btnPujar);
+
+        pBotonera = new JPanel();
+        getContentPane().add(pBotonera, BorderLayout.NORTH);
+        pBotonera.setLayout(new BorderLayout(0, 0));
+
+        btnVolver = new JButton("Volver");
+        pBotonera.add(btnVolver, BorderLayout.WEST);
 
         btnPujar.addActionListener(new ActionListener()
         {
@@ -67,8 +82,9 @@ public class frmPujar extends JFrame
             public void actionPerformed(ActionEvent e)
             {
                 clsPuja puja = new clsPuja(usuario.getEmail(), jugadorSeleccionado.getId(), Double.parseDouble(txtSuma.getText()));
-
                 controller.Pujar(puja);
+                panelUsuario.setVisible(true);
+                dispose();
             }
         });
 
@@ -86,7 +102,6 @@ public class frmPujar extends JFrame
                 clsJugador seleccion = (clsJugador) ((JList)arg0.getSource()).getSelectedValue();
                 btnPujar.setEnabled(true);
             }
-
         };
     }
 }
